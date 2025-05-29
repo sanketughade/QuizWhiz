f47d6cda391be4b1ce8499ea60fc92c5f214e16c
Sanket Ughade <sanketughade96@gmail.com>
2025-05-29 20:49:01 +0530

    Add the launch Screen with Animation

--- QuizWhiz/Controller/SplashScreenViewController.swift
+++ QuizWhiz/Controller/SplashScreenViewController.swift
@@ -5,4 +5,55 @@
 //  Created by Sanket Ughade on 28/05/25.
 //
 
-import Foundation
+import UIKit
+import Lottie
+
+class SplashScreenViewController: UIViewController {
+    
+    private var animationView: LottieAnimationView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(hex: "#1DB954")
+        
+        animationView = LottieAnimationView(name: "launch_animation")
+        animationView?.frame = view.bounds
+        animationView?.contentMode = .scaleAspectFit
+        animationView?.loopMode = .playOnce
+        
+        if let animationView = animationView {
+            view.addSubview(animationView)
+            animationView.play { [weak self] finished in
+                if finished {
+                    self?.goToHomeScreen()
+                }
+            }
+        }
+    }
+    
+    private func goToHomeScreen() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let mainVC = storyboard.instantiateInitialViewController()!
+        mainVC.modalPresentationStyle = .fullScreen
+        self.present(mainVC, animated: true, completion: nil)
+    }
+    
+}
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if hexSanitized.hasPrefix("#") {
+            hexSanitized.removeFirst()
+        }
+
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
