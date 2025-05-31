b48fc6bb7d972dc4be422fc5214b28873ca4080d
Sanket Ughade <sanketughade96@gmail.com>
2025-05-31 19:06:37 +0530

    Reverted some unwanted changes

--- QuizWhiz/Controller/ViewController.swift
+++ QuizWhiz/Controller/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class QuizSetupViewController: UIViewController {
+class ViewController: UIViewController {
     
     //MARK: UI Elements
     
@@ -18,16 +18,19 @@ class QuizSetupViewController: UIViewController {
     let categoryPicker = UIPickerView()
     let difficultyPicker = UIPickerView()
     
+    let startQuizButton = UIButton()
+    
     let categories = ["General Knowledge", "Entertainment", "Science", "History"]
     let difficulties = ["Easy", "Medium", "Hard"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(hex: "#f32e9")
+        view.backgroundColor = UIColor(hex: "#f3f2e9")
         
         setupInput(questionCountField, placeholder: "Number of Questions")
         setupInput(categoryPickerField, placeholder: "Select Category")
         setupInput(difficultyPickerField, placeholder: "Select Difficulty")
+        setupStartQuizButton(startQuizButton)
         
         categoryPickerField.inputView = categoryPicker
         difficultyPickerField.inputView = difficultyPicker
@@ -38,11 +41,18 @@ class QuizSetupViewController: UIViewController {
         difficultyPicker.dataSource = self
         
         layoutInputs()
+        
+        //Add tap animations
+        startQuizButton.addTarget(self, action: #selector(touchDown(_:)), for: .touchDown)
+        startQuizButton.addTarget(self, action: #selector(touchUp(_:)), for: [.touchUpInside, .touchUpOutside, .touchCancel])
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        [questionCountField, categoryPickerField, difficultyPickerField].forEach { applyAsymmetricBorder(to: $0) }
+        
+        DispatchQueue.main.async {
+            [self.questionCountField, self.categoryPickerField, self.difficultyPickerField, self.startQuizButton].forEach { self.applyAsymmetricBorder(to: $0) }
+        }
     }
     
     //MARK: Input Setup
@@ -54,21 +64,57 @@ class QuizSetupViewController: UIViewController {
         field.layer.cornerRadius = 12
         field.borderStyle = .none
         field.delegate = self
-        view.addSubview(field)
+        
+        //Add padding
+        let padding = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 0))
+        field.leftView = padding
+        field.leftViewMode = .always
+        field.rightView = padding
+        field.rightViewMode = .always
     }
+    
+    //MARK: Start Quiz Button
+    func setupStartQuizButton(_ button: UIButton) {
+        button.setTitle("Start Quiz 🚀", for: .normal)
+        button.setTitleColor(UIColor(hex: "#f3f2e9"), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Bangers", size: 22)
+        button.backgroundColor = UIColor(hex: "#b684df")
+        button.layer.masksToBounds = false
+        
+        //Padding inside button text
+        button.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 14, leading: 20, bottom: 14, trailing: 20)
+    }
+    
     
     //MARK: Layout
     func layoutInputs() {
-        let inputs = [questionCountField, categoryPickerField, difficultyPickerField]
+        let stackView = UIStackView(arrangedSubviews: [
+            questionCountField,
+            categoryPickerField,
+            difficultyPickerField,
+            startQuizButton
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 30
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
-        for (i, field) in inputs.enumerated() {
-            field.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                field.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-                field.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
-                field.heightAnchor.constraint(equalToConstant: 55),
-                field.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: CGFloat(60 + i * 80))
-            ])
+        stackView.setCustomSpacing(30, after: questionCountField)
+        stackView.setCustomSpacing(30, after: categoryPickerField)
+        stackView.setCustomSpacing(50, after: difficultyPickerField)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32)
+        ])
+        
+        //Set height constraint for each field
+        [questionCountField, categoryPickerField, difficultyPickerField, startQuizButton].forEach {
+            $0.heightAnchor.constraint(equalToConstant: 55).isActive = true
         }
     }
     
@@ -99,17 +145,31 @@ class QuizSetupViewController: UIViewController {
         
         [top, left, right, bottom].forEach { view.layer.addSublayer($0) }
     }
+    
+    @objc func touchDown(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1) {
+            sender.transform = CGAffineTransform(scaleX: 0.98, y: 0.98)
+            sender.backgroundColor = UIColor(hex: "#a370d3") // slightly darker
+        }
+    }
+    
+    @objc func touchUp(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1) {
+            sender.transform = .identity
+            sender.backgroundColor = UIColor(hex: "#b684df")
+        }
+    }
 
 
 }
 
 //MARK: - UITextFieldDelegate
-extension QuizSetupViewController: UITextFieldDelegate {
+extension ViewController: UITextFieldDelegate {
     
 }
 
 //MARK: - UIPickerViewDelegate
-extension QuizSetupViewController: UIPickerViewDelegate {
+extension ViewController: UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -131,7 +191,7 @@ extension QuizSetupViewController: UIPickerViewDelegate {
     }
 }
 
-extension QuizSetupViewController: UIPickerViewDataSource {
+extension ViewController: UIPickerViewDataSource {
     
 }
 
