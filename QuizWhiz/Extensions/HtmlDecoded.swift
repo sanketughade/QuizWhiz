79fdb383fb3856620f227797b7448aaea8496db1
Sanket Ughade <sanketughade96@gmail.com>
2025-06-22 19:15:00 +0530

    Add the functionality to get question from API call

---
+++ QuizWhiz/Extensions/HtmlDecoded.swift
@@ -0,0 +1,8 @@
+//
+//  HtmlDecoded.swift
+//  QuizWhiz
+//
+//  Created by Sanket Ughade on 22/06/25.
+//
+
+import Foundation
---
+++ QuizWhiz/Model/QuizService.swift
@@ -0,0 +1,8 @@
+//
+//  QuizService.swift
+//  QuizWhiz
+//
+//  Created by Sanket Ughade on 22/06/25.
+//
+
+import Foundation
